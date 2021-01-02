90986b78e3cad8c82dbc177ac0f9b8f83b5be40b
romsaejao <76035596+romsaejao@users.noreply.github.com>
2021-01-02 20:31:03 +0700

    Lab7.3 / completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,58 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter your age: ";
+	cin>> a; // a = age
+	if ( a <= 20)
+	{
+		cout<<"Enter your height: ";
+		cin>> b; // b = height
+		if ( b < 160 )
+		{
+			cout<< "Your status = UNFRIEND";
+		}
+		else if ( b < 175 )
+		{
+			cout<< "Your status = FRIEND";
+		}
+		else 
+		{
+			
+				cout<<"Enter your property: ";
+				cin>> c; // c = your property
+				if ( c > 69000000 )
+				{
+					cout<<"Your status = MARRIED";
+				}
+				else
+				{
+					cout<<"Your status = ONE-NIGHT-STAND";
+				}
+		}
+	}
+		else
+		{
+			if ( a < 30 )
+			{
+				
+				cout<<"Enter your property: ";
+				cin>> c; // c = your property
+				if ( c > 10000000 )
+				{
+					cout<<"Your status = BEST FRIEND";
+				}
+				else
+				{
+					cout<< "Your status = UNFRIEND";
+				}
+			}
+			else 
+			{
+				cout<< "Your status = UNFRIEND";
+			}
+		}
+	
+	
+}
